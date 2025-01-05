9001b3a1a6405983af988201d4d7b4a4e22c6840
DonetskBCN_2022 <lukianets.anastasiia@gmail.com>
2025-01-05 20:07:56 +0100

    Deriving Computed Values From Other Computed Values

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard; // producing some derived state/computed state that is derived from state in the parent component
-
-  // every turn in turns will be an object of this shape: { square: { row: 0, col: 0 }, player: "X" }
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
